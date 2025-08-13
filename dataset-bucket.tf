7975d2410b925f479f27d7fdfd93223c7c97cd3f
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-08-13 08:30:27 +0100

    remove:S3 Action GetObjVersion

--- dataset-bucket.tf
+++ dataset-bucket.tf
@@ -41,8 +41,7 @@ resource "aws_iam_policy" "dataset_bucket_policy" {
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
-          "s3:ListBucket",
-          "s3:GetObjectVersion"
+          "s3:ListBucket"
         ]
         Resource = [
           aws_s3_bucket.dataset_bucket.arn,
